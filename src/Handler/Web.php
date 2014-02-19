e6ab64338ffd243438c89465a2d119977a751966
Adam Pancutt <adam@panadas.co.uk>
2014-02-19 19:09:47 +0000

    Added console

--- src/Handler/Web.php
+++ src/Handler/Web.php
@@ -7,6 +7,7 @@ use Panadas\Framework\Application;
 use Panadas\Framework\ApplicationAwareInterface;
 use Panadas\Framework\ApplicationAwareTrait;
 use Panadas\HttpMessage\HtmlResponse;
+use Panadas\HttpMessage\Request;
 use Panadas\LoggerPlugin\DataStructure\Records;
 use Panadas\LoggerPlugin\Logger;
 use Panadas\LoggerPlugin\Formatter\Web as WebFormatter;
@@ -95,8 +96,10 @@ class Web extends AbstractProcessingHandler implements ApplicationAwareInterface
             return;
         }
 
+        $request = $event->getParams()->get("request");
+
         try {
-            $response->appendContent($this->render($response));
+            $response->appendContent($this->render($request, $response));
         } catch (\Exception $ignore) {
         }
     }
@@ -106,23 +109,34 @@ class Web extends AbstractProcessingHandler implements ApplicationAwareInterface
         return $this->getRecords()->append($record);
     }
 
-    protected function render(HtmlResponse $response)
+    protected function render(Request $request, HtmlResponse $response)
     {
-        $requestTimestamp = $this->getRequestDateTime()->format("U.u");
+        $requestTimestamp = $this->getRequestDateTime();
 
-        $content = "<table class=\"table table-condensed\" id=\"#panadas-web-debug\" width=\"100%\">";
+        $options = [
+            "selector" => "#panadas-console",
+            "timer" => number_format(microtime(true) - $requestTimestamp->format("U.u"), 3),
+            "panels" => [],
+            "speed" => 200
+        ];
+
+        $records = $this->getRecords();
+
+        $content = "<table>";
 
         foreach ($this->getRecords() as $record) {
 
             if ($record["level"] >= Logger::ERROR) {
-                $class = "danger";
+                $class = "panadas-text-error";
             } elseif ($record["level"] >= Logger::WARNING) {
-                $class = "warning";
+                $class = "panadas-text-warning";
+            } elseif ($record["level"] >= Logger::INFO) {
+                $class = "panadas-text-info";
             } else {
-                $class = "success";
+                $class = null;
             }
 
-            $offset = number_format(($record["datetime"]->format("U.u") - $requestTimestamp), 3);
+            $offset = number_format(($record["datetime"]->format("U.u") - $requestTimestamp->format("U.u")), 3);
             $message = $response->esc($record["message"]);
 
             if ($record["context"]) {
@@ -130,20 +144,96 @@ class Web extends AbstractProcessingHandler implements ApplicationAwareInterface
                 foreach ($record["context"] as $key => $value) {
                     $context[] = "{$key}: " . Php::toString($value);
                 }
-                $message .= "<br><span class=\"text-muted\">" . $response->esc(implode("<br>", $context)) . "</span>";
+                $message .= "<br>";
+                $message .= "<small>{$response->esc(implode("<br>", $context))}</small>";
             }
 
-            $content .= "<tr class=\"{$response->escAttr($class)}\">";
-            $content .= "<td><small><kbd>{$response->esc($offset)}s</kbd></small></td>";
-            $content .= "<td><small><kbd>{$response->esc($record["level_name"])}</kbd></small></td>";
-            $content .= "<td width=\"100%\"><small><kbd>{$message}</kbd></small></td>";
+            $content .= "<tr class=\"{$class}\">";
+            $content .= "<td>{$response->esc($offset)}s</td>";
+            $content .= "<td>{$response->esc($record["level_name"])}</td>";
+            $content .= "<td width=\"100%\">{$message}</td>";
             $content .= "</tr>";
 
         }
 
         $content .= "</table>";
 
-        return $content;
+        $options["panels"]["log"] = [
+            "label" => "Log",
+            "counter" => count($records),
+            "content" => $content
+        ];
+
+        $parameters = [
+            [
+               "label" => "Query",
+               "params" => $request->getQueryParams()
+            ],
+            [
+               "label" => "Data",
+               "params" => $request->getDataParams()
+            ],
+            [
+               "label" => "Cookies",
+               "params" => $request->getCookies()
+            ],
+            [
+               "label" => "Server",
+               "params" => $request->getServerParams()
+            ],
+        ];
+
+        $counter = 0;
+        $content = null;
+
+        foreach ($parameters as $paramData) {
+
+            $content .= "<table>";
+            $content .= "<tr>";
+            $content .= "<th colspan=\"2\">";
+            $content .= "{$response->esc($paramData["label"])} ";
+            $content .= "<span class=\"panadas-badge\">{$paramData["params"]->count()}</span>";
+            $content .= "</th>";
+            $content .= "</tr>";
+
+            if ($paramData["params"]->populated()) {
+
+                foreach ($paramData["params"] as $key => $value) {
+                    $content .= "<tr>";
+                    $content .= "<td>{$response->esc($key)}</td>";
+                    $content .= "<td width=\"100%\">{$response->esc($value)}</td>";
+                    $content .= "</tr>";
+                    $counter++;
+                }
+
+            } else {
+
+                $content .= "<tr>";
+                $content .= "<td colspan=\"2\">&mdash;</td>";
+                $content .= "</tr>";
+
+            }
+
+            $content .= "</table>";
+        }
+
+        $options["panels"]["request"] = [
+            "label" => "Request",
+            "counter" => $counter,
+            "content" => $content
+        ];
+
+        return '
+            <link rel="stylesheet" href="/css/panadas.css">
+            <script src="//code.jquery.com/jquery-2.1.0.min.js"></script>
+            <script src="/js/panadas.js"></script>
+            <script>
+                jQuery(function() {
+                    new panadas.console(jQuery, ' . json_encode($options). ');
+                });
+            </script>
+            <div id="' . $response->escAttr(ltrim($options["selector"], "#")) . '"></div>
+        ';
     }
 
     protected function getDefaultFormatter()
